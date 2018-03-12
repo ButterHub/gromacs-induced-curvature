3ed6a8878f2bf090d6a1fff84488b002f45410fc
Adrian Butterworth @ MIT <24711048+ButterHub@users.noreply.github.com>
2018-03-12 13:08:20 -0400

    NPs preliminary

--- analysis/plot.py
+++ analysis/plot.py
@@ -8,11 +8,11 @@ import matplotlib.pyplot as plt
 
 mols = ['methyl-phosphate-mol-pmf.xvg', 'methyl-hydrogen-phosphate-mol-pmf.xvg', 'methyl-dihydrogen-phosphate-mol-pmf.xvg', 'methyl-sulfonate-mol-pmf.xvg']
 ions = ['na-mol-pmf.xvg', 'ca-mol-pmf.xvg']
-nps = ['mus-2nm-sym-pmf.xvg']
+nps = ['mus-2nm-sym-pmf.xvg', 'dopc494-mus-ot-2nm-sym-pmf.xvg', 'dopc398-ribbon-mus-ot-2nm-asym-pmf.xvg']
 water = ['water-pmf.xvg']
 # EDIT 'FILES' ONLY
-files = mols
-name = "methyl-phosphates"
+files = nps
+name = "np"
 
 # Create figure
 fig, ax = plt.subplots(1)
