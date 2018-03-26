4cbbbee6ec41e64582c3ea4bcdaa4f4a4d24e721
Adrian Butterworth @ MIT <24711048+ButterHub@users.noreply.github.com>
2018-03-25 20:12:29 -0400

    PMFs prelim

--- methyl-dihydrogen-phosphate-mol-pmf-large/launch_pulling_16.sh
+++ methyl-dihydrogen-phosphate-mol-pmf-large/launch_pulling_16.sh
@@ -2,7 +2,7 @@
 #SBATCH -J 16-UEYB
 #SBATCH -o pmf-16.out
 #SBATCH -N 1
-#SBATCH -n 64
-#SBATCH -p extended-cpu
+#SBATCH -n 32
+#SBATCH -p extended-mem
 mdrun -v -pf pullf_16.xvg -px pullx_16.xvg -deffnm pulling_16_md -cpi pulling_16_md.cpt
 
--- methyl-hydrogen-phosphate-mol-pmf-large/launch_pulling_19.sh
+++ methyl-hydrogen-phosphate-mol-pmf-large/launch_pulling_19.sh
@@ -2,8 +2,8 @@
 #SBATCH -J 19-VQJG
 #SBATCH -o pmf-19.out
 #SBATCH -N 1
-#SBATCH -n 8
-#SBATCH -p regular-cpu
+#SBATCH -n 32
+#SBATCH -p extended-mem
 
 grompp -f pull_eq_19.mdp -c solution_em.gro -n solution.ndx -p solution.top -o pulling_19_eq -maxwarn 1
 mdrun -v -deffnm pulling_19_eq
