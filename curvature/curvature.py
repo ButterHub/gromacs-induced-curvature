e94b8958901ef4b7ecacdfc20861f4eb75327ee1
Adrian Butterworth @ MIT <24711048+ButterHub@users.noreply.github.com>
2018-06-14 15:57:34 -0400

    latest

--- curvature/curvature.py
+++ curvature/curvature.py
@@ -4,12 +4,12 @@ import MDAnalysis as mda
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit, minimize_scalar, least_squares
 
 # VARIABLES
 DIR = "/old/rvanlehn/planar_insertion/unbiased/musot/ribbon_extended/unb_7_2500ps_1"
 HEAD = "O* P*"
-GRO = "{}/ribbon_musot_7_2500ps_1_400ns_center.gro".format(DIR)
+GRO = "{}/ribbon_musot_7_2500ps_1_400ns.gro".format(DIR)
 XTC = "{}/ribbon_musot_7_2500ps_1_400ns_center.xtc".format(DIR)
 u = mda.Universe(GRO, XTC) # Universe created
 
@@ -17,39 +17,104 @@ u = mda.Universe(GRO, XTC) # Universe created
 def gauss(x, a, b, c):
     return a*np.exp((-(x - b)**2)/(2*c**2)) #+ d
 
+def fit_circle(x, y):
+    # == METHOD 1 ==
+    method_1 = 'algebraic'
+    
+    # coordinates of the barycenter
+    x_m = np.mean(x)
+    y_m = np.mean(y)
+    
+    # calculation of the reduced coordinates
+    u = x - x_m
+    v = y - y_m
+    
+    # linear system defining the center (uc, vc) in reduced coordinates:
+    #    Suu * uc +  Suv * vc = (Suuu + Suvv)/2
+    #    Suv * uc +  Svv * vc = (Suuv + Svvv)/2
+    Suv  = sum(u*v)
+    Suu  = sum(u**2)
+    Svv  = sum(v**2)
+    Suuv = sum(u**2 * v)
+    Suvv = sum(u * v**2)
+    Suuu = sum(u**3)
+    Svvv = sum(v**3)
+    
+    # Solving the linear system
+    A = np.array([ [ Suu, Suv ], [Suv, Svv]])
+    B = np.array([ Suuu + Suvv, Svvv + Suuv ])/2.0
+    uc, vc = np.linalg.solve(A, B)
+    
+    xc_1 = x_m + uc
+    yc_1 = y_m + vc
+    
+    # Calcul des distances au centre (xc_1, yc_1)
+    Ri_1     = np.sqrt((x-xc_1)**2 + (y-yc_1)**2)
+    R_1      = np.mean(Ri_1)
+    residu_1 = np.sum((Ri_1-R_1)**2)
+
+    return R_1
+
+def fit_circle_2(x, y):
+    
+    def calc_R(xc, yc):
+        """ calculate the distance of each 2D points from the center (xc, yc) """
+        return np.sqrt((x-xc)**2 + (y-yc)**2)
+    
+    def f_2(c):
+        """ calculate the algebraic distance between the data points and the mean circle centered at c=(xc, yc) """
+        Ri = calc_R(*c)
+        return Ri - Ri.mean()
+    
+    x_m = np.mean(x)
+    y_m = np.mean(y)
+
+    center_estimate = x_m, y_m
+    res = least_squares(f_2, center_estimate)
+    
+    center_2 = res.x
+    xc_2, yc_2 = center_2
+    Ri_2       = calc_R(*center_2)
+    R_2        = Ri_2.mean()
+    residu_2   = np.sum((Ri_2 - R_2)**2)
+
+    return R_2
+
 # ARRAYS
 xU = np.array([])
 zxU = np.array([])
 yU = np.array([])
 zyU = np.array([])
 curvatureXZ = np.array([])
-curvatureYZ = np.array([])
+#curvatureYZ = np.array([])
 time = np.array([])
 
 # Data
 for ts in u.trajectory:
-    print ts.frame
-    lipids_com = u.select_atoms("resname DOPC").center_of_mass()
-    u_heads = u.select_atoms("resname DOPC and prop x>65 and prop x<165 and prop z>{} and name {}".format(lipids_com[2],HEAD))
-    l_heads = u.select_atoms("resname DOPC and prop x>65 and prop x<165 and prop z<{} and name {}".format(lipids_com[2],HEAD))
+    if ts.frame > 400:
+        break
+    #lipids_com = u.select_atoms("resname DOPC").center_of_mass()
+    #u_heads = u.select_atoms("resname DOPC and prop x>65 and prop x<165 and prop z>{}".format(lipids_com[2],HEAD))
+    l_heads = u.select_atoms("resname DOPC")
     # u_heads_pos = u_heads.positions
     l_heads_pos = l_heads.positions 
     x = [sublist[0] for sublist in l_heads_pos]
     y = [sublist[1] for sublist in l_heads_pos]
     z = [sublist[2] for sublist in l_heads_pos]
     # Gaussian fit, (X,Z) and (Y,Z)  
-    p_initial = [1.0, 115.0, 0.1]
+    #p_initial = [1.0, 1.0, 1.0]
     # TODO Fix negative variances and failed calculations
-    popt_UXZ, pcov_UXZ = curve_fit(gauss, x, z, p0=p_initial, maxfev = 10000000)
-    popt_UYZ, pcov_UYZ = curve_fit(gauss, y, z, p0=p_initial, maxfev = 10000000)
-    result_UXZ = popt_UXZ[2] #2 is variance. Or can switch to 2nd derivative at peak.
-    result_UYZ = popt_UYZ[2]
+    #popt_UXZ, pcov_UXZ = curve_fit(gauss, x, z, p0=p_initial, maxfev = 1000000)
+    #popt_UYZ, pcov_UYZ = curve_fit(gauss, y, z, p0=p_initial, maxfev = 10000000)
+    result_UXZ = 1./fit_circle_2(x,z) #2 is variance. Or can switch to 2nd derivative at peak.
+    print ts.frame, result_UXZ
+    #result_UYZ = popt_UYZ[2]
     # Save to curvature array.
     curvatureXZ = np.append(curvatureXZ, result_UXZ)
-    curvatureYZ = np.append(curvatureYZ, result_UYZ)
+    #curvatureYZ = np.append(curvatureYZ, result_UYZ)
     time = np.append(time, ts.time) #time in ps
 
-np.savetxt('Lower_Curvature.txt', np.c[time, curvatureXZ, curvatureYZ])
+np.savetxt('Lower_Curvature.txt', np.c_[time, curvatureXZ])
 # plt.plot(time, curvatureXZ, 'rx', label='data')
 # plt.plot(x, z, 'rx')
 # plt.plot(time, curvatureYZ)
--- curvature/curvature_mukarram.py
+++ curvature/curvature_mukarram.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-DIR = "/old/rvanlehn/planar_insertion/unbiased/musot/ribbon_extended/unb_7_2500ps_1"
-GRO = "{}/ribbon_musot_7_2500ps_1_400ns_center.gro".format(DIR)
-XTC = "{}/ribbon_musot_7_2500ps_1_400ns_center.xtc".format(DIR)
+DIR = "/old/rvanlehn/planar_insertion/control/musot"
+GRO = "{}/ribbon_musot_middle_114200.gro".format(DIR)
+XTC = "{}/ribbon_musot_middle_400ns.xtc".format(DIR)
 
 def get_bottom_layer(coords, dx = 10, max_x = 300):
   bottom_coords = []
@@ -27,35 +27,33 @@ def get_bottom_layer(coords, dx = 10, max_x = 300):
   return bottom_coords[bottom_coords[:,0].argsort()] #Sorted by first column
 
 #Create universe
-u = MDAnalysis.Universe(GRO, GRO)
+u = MDAnalysis.Universe(GRO, XTC)
 
-#Get center of gold nanoparticle x coordinate
-au = u.select_atoms("resname AU")
-cog_x = au.center_of_geometry()[0]
-
-#Select x-z coordinates of all phosphate groups in DOPC
-phos = u.select_atoms("resname DOPC and name O* P*")
-phos_array = np.array(phos.positions)
-phos_array = np.delete(phos_array, 1, 1)
-
-phos_lower = get_bottom_layer(phos_array)
-
-#Fit quartic curve to bottom of bilayer
-phos_x = [x[0] for x in phos_lower]
-phos_z = [x[1] for x in phos_lower]
-
-paras = np.polyfit(phos_x, phos_z, 6)
-p = np.poly1d(paras)
-phos_full_x = [x[0] for x in phos_array]
-phos_full_z = [x[1] for x in phos_array]
-xp = np.linspace(0, 200, 200)
-_ = plt.plot(phos_full_x, phos_full_z, '.', xp, p(xp), 'r-', [cog_x], [p(cog_x)], 'yo')
-plt.ylim(0, 200)
-
-#Print curvature
-curvature = p.deriv(2)
-print curvature(cog_x)
-
-#plt.scatter(phos_x, phos_z)
-plt.show()
-
+for ts in u.trajectory:
+  if ts.frame > 1000:
+    break
+  #Get center of gold nanoparticle x coordinate
+  au = u.select_atoms("resname AU")
+  cog_x = au.center_of_geometry()[0]
+  
+  #Select x-z coordinates of all phosphate groups in DOPC
+  phos = u.select_atoms("resname DOPC and name O* P*")
+  phos_array = np.array(phos.positions)
+  phos_array = np.delete(phos_array, 1, 1)
+  
+  phos_lower = get_bottom_layer(phos_array)
+  
+  #Fit quartic curve to bottom of bilayer
+  phos_x = [x[0] for x in phos_lower]
+  phos_z = [x[1] for x in phos_lower]
+  
+  paras = np.polyfit(phos_x, phos_z, 6)
+  p = np.poly1d(paras)
+  phos_full_x = [x[0] for x in phos_array]
+  phos_full_z = [x[1] for x in phos_array]
+  xp = np.linspace(0, 200, 200)
+  
+  #Print curvature
+  curvature = p.deriv(2)
+  print ts.frame, abs(curvature(cog_x))
+  
